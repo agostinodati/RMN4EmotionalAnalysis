0e2705836f2c4a3f583de42a7540a737051f0370
agostinodati <agostino.dati@outlook.it>
2021-07-31 11:44:59 +0200

    Implemented a dir walker

--- RMN4EmotionalAnalysis/main.py
+++ RMN4EmotionalAnalysis/main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from rmn import RMN
+import os
 from os import listdir
 from os.path import isfile, join
 
@@ -22,10 +23,39 @@ def main_test():
         image = m.draw(image, results)
         cv2.imwrite('examples/results/output_' + str(count) + '.png', image)
         count += 1
-    emo_voted = max(emo_dict, key=emo_dict.get)
-    votes = max(emo_dict.values())
+    emo_voted = max(emo_dict, key=emo_dict.get) # NB: se ci sono più elementi max, prende il primo che incontra. Loggare quando vi sono più max eventualmente
+    votes = emo_dict[emo_voted]
     print('Emotion voted is ' + emo_voted + ' with '+str(votes) + ' votes')
 
 
+def main():
+    '''
+    Per ogni sottocartella X di "cohn-kanade-images", devo aprire ogni sotto cartella Y e votare le immagini contenute.
+    Fatto ciò, verifico il risultato andando a prendere corrispendente dentro la sottocartella Y dentro "Emotion_labels".
+    Quindi conviene avere due liste con tutte le sottocartelle del dataset. Oppure modificare i nomi delle sotto-sottocartelle
+    effettuando un po' di preprocessing
+    '''
+    pathDataset = 'D:/CKdataset/cohn-kanade-images/'
+    pathLabelDir = 'D:/CKdataset/Emotion_labels/Emotion/'
+    listDir = os.listdir(pathDataset)
+    for dir in listDir:
+        pathDir = pathDataset + dir + '/'
+        if os.path.isdir(pathDir):
+            listSubDir = os.listdir(pathDir)
+            for subdir in listSubDir:
+                pathSubDir = pathDir + subdir + '/'
+                if os.path.isdir(pathSubDir) and subdir != '.DS_Store':
+                    listFrames = os.listdir(pathSubDir)
+                    for frame in listFrames:
+                        print(pathSubDir + frame)
+                pathLabel = pathLabelDir + dir + '/' + subdir + '/'
+                if os.path.isdir(pathLabel):
+                    emotion_label_filepath = pathLabel + os.listdir(pathLabel)[0]
+                    print(emotion_label_filepath)
+                    with open(pathLabel, 'r') as f:
+                        label = f.readline()
+
+
+
 if __name__ == '__main__':
-    main_test()
+    main()
